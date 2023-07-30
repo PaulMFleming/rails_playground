ff210c079396f865673d387081da4b28345dcde8
Paul M Fleming <paul.stillbreathing@gmail.com>
2023-07-30 15:30:03 +0100

    add public method public_count to Visible concern

--- blog/app/controllers/concerns/visible.rb
+++ blog/app/controllers/concerns/visible.rb
@@ -5,8 +5,14 @@ module
 
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
-  end`
-  `
+  end
+
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+  
   def archived?
     status == 'archived'
   end
