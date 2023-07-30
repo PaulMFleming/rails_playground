4da05023b9a57dcab73c7c480ae40d623741e06f
Paul M Fleming <paul.stillbreathing@gmail.com>
2023-07-30 13:42:06 +0100

    create article view & controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,6 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(parama[:id])
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article  = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
 end
